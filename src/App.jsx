654019207625f36b4108d294fe4b5d60d86a34a0
ndk1195 <ndkhoa1195@gmail.com>
2024-12-18 14:23:23 +0700

    add searchbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import SearchBox from "./components/SearchBox";
 import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
     <div className="grid min-h-dvh place-items-center bg-#F6F8FF dark:bg-#141D2F">
-      <ThemeToggle />
+      {/* container */}
+      <div className="w-full max-w-[730px] px-6 py-8 xl:p-0">
+        {/* header */}
+        <div className="mb-[35px] flex items-center justify-between">
+          <h1 className="text-[26px] font-bold leading-none text-[#222731] dark:text-white">
+            devfinder
+          </h1>
+          <ThemeToggle />
+        </div>
+        {/* header */}
+
+        {/* search box */}
+        <SearchBox />
+        {/* search box */}
+      </div>
+      {/* container */}
     </div>
   );
 }
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,34 @@
+import iconSearch from "../assets/icon-search.svg";
+function SearchBox() {
+  function handleSearch() {}
+  return (
+    <div className="w-full rounded-[15px] bg-#FEFEFE py-[7px] pl-4 pr-[7px] shadow-box md:py-[9.5px] md:pl-8 md:pr-[10px] dark:bg-#1E2A47 dark:shadow-none">
+      <form
+        className="flex w-full items-center justify-between"
+        onSubmit={handleSearch}
+      >
+        <div className="flex items-center gap-2 md:gap-6">
+          <img src={iconSearch} alt="icon search" className="w-5 md:w-6" />
+          <input
+            type="text"
+            className="w-[200px] bg-white text-[13px] leading-[25px] text-[#222731] caret-#0079FF outline-none placeholder:text-#4B6A9B md:w-[270px] md:text-lg dark:bg-#1E2A47 dark:text-white placeholder:dark:text-white"
+            placeholder="Search GitHub username…"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-2 sm:flex-row md:gap-6">
+          <p className="text-[13px] font-bold leading-none text-[#F74646] md:text-[15px]">
+            No results
+          </p>
+
+          <button
+            className="flex h-[46px] w-[84px] items-center justify-center rounded-[10px] bg-#0079FF text-sm font-bold leading-none text-white transition-colors hover:bg-[#60ABFF] md:h-[50px] md:w-[106px]"
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default SearchBox;
